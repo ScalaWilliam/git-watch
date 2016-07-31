c04b0fb8d488e198eab72dc1ff4e619deb30a284
Vykintas Narmontas (William) <william@scalawilliam.com>
2016-07-31 11:22:33 +0800

    Basic flow works. Closes #1

--- app/controllers/GitHub.scala
+++ app/controllers/GitHub.scala
@@ -2,15 +2,43 @@ package controllers
 
 import javax.inject.{Inject, Singleton}
 
-import play.api.mvc.Controller
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.Source
+import play.api.inject.ApplicationLifecycle
+import play.api.libs.EventSource.Event
+import play.api.libs.iteratee.Concurrent
+import play.api.libs.streams.Streams
+import play.api.mvc.{Action, BodyParsers, Controller}
+
+import scala.concurrent.{ExecutionContext, Future}
 
 /**
   * Created by me on 31/07/2016.
   */
 
 @Singleton
-class GitHub @Inject()() extends Controller {
-  def push(id: String) = TODO
+class GitHub @Inject()(applicationLifecycle: ApplicationLifecycle)(implicit actorSystem: ActorSystem, executionContext: ExecutionContext) extends Controller {
 
-  def get(id: String) = TODO
+  val (enum, channel) = Concurrent.broadcast[Event]
+
+  import concurrent.duration._
+
+  val keepAlive = actorSystem.scheduler.schedule(5.seconds, 10.seconds) {
+    channel.push(Event(data = "", id = None, name = None))
+  }
+
+  applicationLifecycle.addStopHook(() => Future.successful(keepAlive.cancel()))
+
+  def source = Source.fromPublisher(Streams.enumeratorToPublisher(enum))
+
+  def push = Action(BodyParsers.parse.json) { request =>
+    channel.push(Event(data = request.body.toString, id = None, name = None))
+    Ok("Got it, thanks.")
+  }
+
+  def watch(owner: String, repo: String) = Action {
+    Ok.chunked(
+      content = source
+    ).as("text/event-stream")
+  }
 }
