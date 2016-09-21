27295930cdc0293838b354a6e6fec892ade2ce62
Vykintas Narmontas (William) <william@scalawilliam.com>
2016-09-21 20:39:33 +0800

    Load index from this repo.

--- app/controllers/Main.scala
+++ app/controllers/Main.scala
@@ -1,60 +1,27 @@
 package controllers
 
+import java.nio.file.Paths
 import javax.inject.{Inject, Singleton}
 
-import akka.agent.Agent
 import play.api.libs.ws.WSClient
 import play.api.mvc.{Action, Controller}
+import play.api.{Environment, Mode}
 import play.twirl.api.Html
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
 
 /**
   * Created by me on 18/08/2016.
   */
 @Singleton
-class Main @Inject()(wsClient: WSClient)(implicit executionContext: ExecutionContext)
+class Main @Inject()(wsClient: WSClient, environment: Environment)(implicit executionContext: ExecutionContext)
   extends Controller {
-  val homepage = Agent(Option.empty[(String, String)])
 
-  def q = controllers.GitHub.buildWatcher _
+  def templatesPath = if (environment.mode == Mode.Prod) Paths.get("templates") else Paths.get("dist", "templates")
 
-  /**
-    * Fastly seems to cache even the If-None-Match stuff, ie it's not invalidating properly
-    * per leaf node...
-    */
-  def theUrl =
-  s"""https://gist.githubusercontent.com/ScalaWilliam/7c29a47e081a537605eef430a25f02b1/raw/git.watch.html?ignore=${scala.util.Random.nextInt()}"""
+  def indexPath = templatesPath.resolve("index.html")
 
-  def fetchPage(caching: Boolean): Future[String] = {
-    homepage.get() match {
-      case Some((etag, content)) if caching =>
-        wsClient.url(theUrl).withHeaders("If-None-Match" -> etag).get.map { resp =>
-          if (resp.status == 304) content
-          else {
-            val etag = resp.header("ETag").get
-            val content = resp.body
-            homepage.send(Some(etag -> content))
-            content
-          }
-        }
-      case _ =>
-        wsClient.url(theUrl).get().map { response =>
-          val etag = response.header("ETag").get
-          val content = response.body
-          homepage.send(Some(etag -> content))
-          content
-        }
-    }
-  }
-
-  def makeNoCache = Action { req =>
-    Ok("Good").withSession("no-cache" -> "true")
-  }
-
-  def index = Action.async { req =>
-    fetchPage(caching = req.headers.get("cache").contains("false") || !req.session.get("no-cache").contains("true")).map { content =>
-      Ok(Html(content))
-    }
+  def index = Action {
+    Ok(Html(scala.io.Source.fromFile(indexPath.toFile).mkString))
   }
 }
