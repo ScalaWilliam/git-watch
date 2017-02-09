e3600a3ee301b93dbe2c2e32dd7967fa4a7bc0b2
Vykintas Narmontas (William) <william@scalawilliam.com>
2017-02-09 21:44:17 +0800

    Simplify just for the happy path; minimise customization; add timestamps to output.

--- client/bin/git-watch.js
+++ client/bin/git-watch.js
@@ -1,41 +1,48 @@
 #! /usr/bin/env node
 var EventSource = require('eventsource');
 var shelljs = require("shelljs");
-var argv = require('yargs').default({
-    "url": 'https://git.watch/events/',
-    "execute": 'make push'
+var argv = require('yargs').boolean('i').default({
+    "u": 'https://git.watch/events/',
+    "x": 'make push',
+    "i": true
 }).argv;
+require('console-stamp')(console);
+
+var executeCommand = argv.x;
 
 var url = require("../lib/url.js");
 
 var repositoryUrl;
-if (!repositoryUrl) {
-    var r = shelljs.exec('git config --get remote.origin.url', {silent: true});
-    if (r.code == 0) {
-        repositoryUrl = url.transform(r.stdout.replace("\n", "").replace("\r", ""));
-    }
+var getUrlResult = shelljs.exec('git config --get remote.origin.url', {silent: true});
+if (getUrlResult.code == 0) {
+    var cleanUrl = getUrlResult.stdout.replace("\n", "").replace("\r", "");
+    repositoryUrl = url.transform(cleanUrl);
 }
 
-console.log("got... ", repositoryUrl);
 if (!repositoryUrl) {
-    console.warn("Repository URL not detected. Will trigger for every push event.");
-    console.warn("We support GitHub, BitBucket and GitLab repositories only.");
-} else {
-    console.log("Matching all events for '" + repositoryUrl + "'");
+    throw new Error("Repository URL not detected.");
 }
 
-var es = new EventSource(argv.url);
+function execute() {
+    console.log("Executing: '" + executeCommand + "'");
+    shelljs.exec(executeCommand);
+}
+
+if (argv['initial-execute']) {
+    console.log("Executing initial command. '-i' to disable.");
+    execute();
+}
+
+var es = new EventSource(argv.u);
 es.onopen = function (e) {
-    console.log(e);
+    console.log("Opened connection to '" + argv.u + "'");
 };
 es.onerror = function (e) {
-    console.error(e);
+    console.error("Connection error", e);
 };
 es.addEventListener('push', function (e) {
-    if (!repositoryUrl || e.data == repositoryUrl) {
-        console.log("Received event: ", e);
-        console.log("Repository URL: ", repositoryUrl);
-        console.log("Executing: " + argv.execute);
-        shelljs.exec(argv.execute);
+    if (e.data == repositoryUrl) {
+        console.log("Received an event for '" + repositoryUrl + "'");
+        execute();
     }
 });
