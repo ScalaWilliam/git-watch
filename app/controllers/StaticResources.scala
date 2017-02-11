e59f6a6bdcde563aac21ef5284833ec28cdab0fa
Vykintas Narmontas (William) <william@scalawilliam.com>
2017-02-11 12:14:35 +0800

    StaticDev -> StaticResources.

--- app/controllers/StaticResources.scala
+++ app/controllers/StaticResources.scala
@@ -7,16 +7,16 @@ import javax.inject.Inject
 import lib.ContentPath
 import play.Environment
 import play.api.mvc.{Action, Controller}
+import scala.collection.JavaConverters._
 
 /**
   * Serve static files for development since we don't want to run nginx on dev machine.
   */
-class StaticDev @Inject()(environment: Environment, contentPath: ContentPath) extends Controller {
+class StaticResources @Inject()(environment: Environment, contentPath: ContentPath) extends Controller {
 
   private def staticPath = contentPath.contentPath.resolve("static")
 
   private def resources = {
-    import scala.collection.JavaConverters._
     Files.list(staticPath).collect(Collectors.toList[Path]).asScala.toList
   }
 
