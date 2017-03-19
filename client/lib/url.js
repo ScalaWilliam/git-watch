2eb4dcf236e9500a9fdf9e3e5d10e704b70fd017
Vykintas Narmontas (William) <william@scalawilliam.com>
2017-03-19 08:27:29 +0800

    Move client to http://github.com/ScalaWilliam/git-watch-client

--- client/lib/url.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    'transform': function (url) {
-        return "https://" + url.replace(".git", "").replace("git@", "").replace("git://", "").replace("https://", "").replace(":", "/");
-    }
-};
